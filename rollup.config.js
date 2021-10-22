f8a32f02b34421150e8f41e89fffec46b60e9b55
Can Burak S <canburak@msn.com>
2021-10-23 02:41:58 +0300

    Browser update

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ export default [
 	{
 		input: 'src/main.js',
 		output: {
-			name: 'howLongUntilLunch',
+			name: 'TextAlign',
 			file: pkg.browser,
 			format: 'umd'
 		},
--- src/main.js
+++ src/main.js
@@ -1,59 +1,83 @@
 class TextAlign {
-    
+    static leftAlignedIcon = '<path d="M17 9.5H3M21 4.5H3M21 14.5H3M17 19.5H3"/>'
+    static centerAlignedIcon = '<path d="M19 9.5H5M21 4.5H3M21 14.5H3M19 19.5H5"/>'
+    static rightAlignedIcon = '<path d="M21 9.5H7M21 4.5H3M21 14.5H3M21 19.5H7"/>' //'<path d="M17 9.5H3M21 4.5H3M21 14.5H3M17 19.5H3"/>'
+    static justifyAlignedIcon = '<path d="M21 9.5H3M21 4.5H3M21 14.5H3M21 19.5H3"/>'
+
     static get isInline() {
         return true;
     }
-    constructor() {
-        this.options = ["left", "center", "right"]
-        this.currenticon = '<svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-width="2" stroke-linecap="square" stroke-linejoin="arcs"><path d="M19 9.5H5M21 4.5H3M21 14.5H3M19 19.5H5"/></svg>'
+    constructor({ api }) {
+        //console.log("Constructing")
+        this.currenticon = '<svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-width="2" stroke-linecap="square" stroke-linejoin="arcs"></svg>'
         this.aligncurrenticon = new DOMParser().parseFromString(this.currenticon,'application/xml');
         this.button = null;
         this.state = "left";
+        this.api = api
     }
 
     render() {
         this.button = document.createElement('button');
-        //this.button.classList.add('align-text');
         this.button.classList = 'ce-inline-tool ce-inline-tool--align-text';
         this.button.appendChild(this.button.ownerDocument.importNode(this.aligncurrenticon.documentElement, true))
+        this.setIcon()
         return this.button;
     }
 
     surround(range) {
-        console.log("htis", this)
-        //console.log("range: ", range.__proto__, range)
+        //console.log("Surrounding")
+        const firstParentNode = this.getParentNode(range.commonAncestorContainer)
         if (this.state === "left") {
-            range.commonAncestorContainer.parentNode.style.textAlign = "center"
+            firstParentNode.style.textAlign = "center"
             this.state = "center"
-            this.button.childNodes[0].innerHTML = '<path d="M21 9.5H7M21 4.5H3M21 14.5H3M21 19.5H7"/>'
-
         }
         else if (this.state === "center") {
-            range.commonAncestorContainer.parentNode.style.textAlign = "right"
+            firstParentNode.style.textAlign = "right"
             this.state = "right"
-            this.button.childNodes[0].innerHTML = '<path d="M21 9.5H3M21 4.5H3M21 14.5H3M21 19.5H3"/>'
         }
         else if (this.state === "right") {
-            range.commonAncestorContainer.parentNode.style.textAlign = "justify"
+            firstParentNode.style.textAlign = "justify"
             this.state = "justify"
-            this.button.childNodes[0].innerHTML = '<path d="M17 9.5H3M21 4.5H3M21 14.5H3M17 19.5H3"/>'
         }
         else if (this.state === "justify") {
-            range.commonAncestorContainer.parentNode.style.textAlign = "left"
+            firstParentNode.style.textAlign = "left"
             this.state = "left"
-            this.button.childNodes[0].innerHTML = '<path d="M19 9.5H5M21 4.5H3M21 14.5H3M19 19.5H5"/>'
         }
     }
 
     checkState(text){
-    console.log("anchor node: ", text)
         if (!text) {
             return;
         }
+        this.setIcon()
     }
 
+    // Find parent node until it is DIV or Paragraph
+    getParentNode(node){
+        if (node?.parentNode?.tagName === "DIV" || node?.parentNode?.tagName === "p") {
+            return node.parentNode
+        }
+        else {
+            return this.getParentNode(node.parentNode)
+        }
+
+    }
+
+    setIcon(){
+        if (this.state === "" || this.state === "left"){
+            this.button.childNodes[0].innerHTML = TextAlign.leftAlignedIcon
+        }
+        else if (this.state === "center"){
+            this.button.childNodes[0].innerHTML = TextAlign.centerAlignedIcon
+        }
+        else if (this.state === "right"){
+            this.button.childNodes[0].innerHTML = TextAlign.rightAlignedIcon
+        }
+        else if (this.state === "justify"){
+            this.button.childNodes[0].innerHTML = TextAlign.justifyAlignedIcon
+        }
+    }
 }
 
 
 export default TextAlign;
-
